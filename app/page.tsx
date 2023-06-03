d251bd000db93422e75510be09a09929cbc2db04
emoltz <es5541@nyu.edu>
2023-06-03 19:36:35 -0400

    ui installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
 import RootLayout from "@/app/layout";
+import {Container, Text} from "@nextui-org/react";
 
 export default function Home() {
     return (
         <>
             <RootLayout>
                 <div className={styles.container}>
-                    Hello world!
+                    <Text h1>
+                        Hello world!
+                    </Text>
                 </div>
             </RootLayout>
         </>
