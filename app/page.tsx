8bf666c99ea8102bece656fb31685b0d8dd18aef
emoltz <es5541@nyu.edu>
2023-06-03 19:01:43 -0400

    nextjs ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+// import Image from 'next/image'
+// import styles from './page.module.css'
 import RootLayout from "@/app/layout";
+import {Text} from "@nextui-org/react";
 
 export default function Home() {
     return (
         <>
             <RootLayout>
-                <div className={styles.container}>
-                    Hello world!
-                </div>
+                <Text
+                    h2
+                    size={30}
+                    css={{
+                        textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                    }}
+                > Hello world!</Text>
             </RootLayout>
         </>
     )
