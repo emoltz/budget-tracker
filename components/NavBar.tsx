42d5baf18d16ad0009b8bfa1f523167ee7329c07
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-22 21:22:16 -0400

    dark mode works on navbar!

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -18,6 +18,7 @@ import {usePathname} from 'next/navigation'
 import {auth} from "@/lib/firebase";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 import NavItem from '@/components/NavItem';
+import {useMantineColorScheme} from "@mantine/core";
 
 const data = [
     {link: '/', label: 'Dashboard', icon: IconDashboard},
@@ -32,6 +33,11 @@ export default function NavBar() {
     const pathname = usePathname();
     const animationDuration = 100;
     const [animationCompleted, setAnimationCompleted] = useState(true);
+    const {colorScheme} = useMantineColorScheme();
+
+    useEffect(() => {
+        console.log(colorScheme);
+    }, [colorScheme]);
 
     useEffect(() => {
         setCollapsed(window.innerWidth < 640)
@@ -46,79 +52,83 @@ export default function NavBar() {
     }, [animationDuration, collapsed])
 
     return (
-        <aside className={`flex relative top-0 z-10 border-r ${collapsed ? "w-[4.5rem]" : "w-72"} transition-all`}>
-            <nav className="flex flex-col m-3 gap-2 divide-y w-full">
-                <div className="flex flex-row px-2 py-1 gap-3 items-center">
-                    {animationCompleted && !collapsed &&
-                        <Link href="/"
-                              className={`text-2xl font-bold font-mono justify-self-start transition-all`}
-                        >Argonaut
-                        </Link>}
+        <div className={`${colorScheme == 'dark' ? "bg-slate-900" : ""}`}>
 
-                    {animationCompleted && !collapsed &&
-                        <Link href="/"
-                              className="text-xs font-mono bg-slate-200 rounded-sm p-1"
-                        >v0.2
-                        </Link>}
+            <aside
+                className={`flex relative ${colorScheme == 'dark' ? "dark" : ""}  top-0 z-10 border-r ${collapsed ? "w-[4.5rem]" : "w-72"} transition-all`}>
+                <nav className="flex flex-col m-3 gap-2 divide-y w-full dark:bg-slate-900">
+                    <div className="flex flex-row px-2 py-1 gap-3 items-center">
+                        {animationCompleted && !collapsed &&
+                            <Link href="/"
+                                  className={`${colorScheme == 'dark'? "text-amber-50" : ""} text-2xl font-bold font-mono justify-self-start transition-all`}
+                            >Argonaut
+                            </Link>}
 
-                    {animationCompleted && !collapsed &&
-                        <ThemeSwitcher/>
-                    }
-                    <div
-                        className={`cursor-pointer grow items-center`}
-                        onClick={() => {
-                            setCollapsed(!collapsed);
-                            setAnimationCompleted(false);
-                        }}
-                    >
-                        <IconArrowsExchange
-                            color={'black'}
-                        />
+                        {animationCompleted && !collapsed &&
+                            <Link href="/"
+                                  className={`text-xs font-mono  rounded-sm p-1 ${colorScheme == 'dark'? "text-amber-50" : ""} justify-self-start transition-all`}
+                            >v0.2
+                            </Link>}
+
+                        {animationCompleted && !collapsed &&
+                            <ThemeSwitcher/>
+                        }
+                        <div
+                            className={`cursor-pointer grow items-center `}
+                            onClick={() => {
+                                setCollapsed(!collapsed);
+                                setAnimationCompleted(false);
+                            }}
+                        >
+                            <IconArrowsExchange
+                                color={`${colorScheme == 'dark' ? 'white' : 'gray'}`}
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <div className="flex flex-col gap-y-3 pt-3">
-                    {data.map((item) => (
-                        <div key={item.label}>
+                    <div className="flex flex-col gap-y-3 pt-3">
+                        {data.map((item) => (
+                            <div key={item.label}>
+                                <NavItem
+                                    name={item.label}
+                                    Icon={item.icon}
+                                    href={item.link}
+                                    key={item.label}
+                                    isActive={pathname.endsWith(item.link)}
+                                    collapsed={collapsed}
+                                />
+                                {/* TODO: .endsWith won't work for dynamic paths */}
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="flex flex-col grow-1 mt-auto gap-y-3 pt-3">
+                        <div>
                             <NavItem
-                                name={item.label}
-                                Icon={item.icon}
-                                href={item.link}
-                                key={item.label}
-                                isActive={pathname.endsWith(item.link)}
+                                name="My Profile"
+                                Icon={IconFingerprint}
+                                href={"/profile"}
+                                isActive={pathname.startsWith("/profile")}
                                 collapsed={collapsed}
                             />
-                            {/* TODO: .endsWith won't work for dynamic paths */}
                         </div>
-                    ))}
-                </div>
 
-                <div className="flex flex-col grow-1 mt-auto gap-y-3 pt-3">
-                    <div>
-                        <NavItem
-                            name="My Profile"
-                            Icon={IconFingerprint}
-                            href={"/profile"}
-                            isActive={pathname.startsWith("/profile")}
-                            collapsed={collapsed}
-                        />
+                        <div onClick={() => {
+                            auth!.signOut();
+                            console.log("logged out");
+                        }}>
+                            <NavItem
+                                name="Logout"
+                                Icon={IconLogout}
+                                href={"/login"}
+                                isActive={pathname.startsWith("/debug")}
+                                collapsed={collapsed}
+                            />
+                        </div>
+
                     </div>
-
-                    <div onClick={() => {
-                        auth!.signOut();
-                        console.log("logged out");
-                    }}>
-                        <NavItem
-                            name="Logout"
-                            Icon={IconLogout}
-                            href={"/login"}
-                            isActive={pathname.startsWith("/debug")}
-                            collapsed={collapsed}
-                        />
-                    </div>
-
-                </div>
-            </nav>
-        </aside>
+                </nav>
+            </aside>
+        </div>
     )
 }
--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -1,28 +1,34 @@
 "use client";
 import Link from 'next/link';
 import React from "react";
+import {useMantineColorScheme} from "@mantine/core";
 
 interface Props {
-  name: string;
-  Icon: React.ElementType;
-  href: string;
-  isActive?: boolean;
-  collapsed: boolean;
-  // children: React.ReactNode;
+    name: string;
+    Icon: React.ElementType;
+    href: string;
+    isActive?: boolean;
+    collapsed: boolean;
+    // children: React.ReactNode;
 }
 
-export default function NavItem({name, Icon, href, isActive, collapsed}:Props) {
-  return (
-      <Link
-        href={href}
-        className={`flex flex-row py-2 px-3 gap-x-3 items-center rounded-md ${isActive && 'bg-slate-300' }`}
-      >
-        <Icon 
-          stroke={1.5}
-          color={"gray"}
-        />
-        {!collapsed && <span className="text-sm grow">{name}</span>}
-        {/*{children}*/}
-      </Link>
-  )
+export default function NavItem({name, Icon, href, isActive, collapsed}: Props) {
+    const {colorScheme} = useMantineColorScheme();
+    const selectedLight = 'bg-slate-300';
+    const selectedDark = 'bg-slate-700';
+    return (
+        <Link
+            href={href}
+            className={`flex flex-row py-2 px-3 gap-x-3 items-center rounded-md ${isActive && (colorScheme == 'dark'? selectedDark : selectedLight)}`}
+        >
+            <Icon
+                stroke={1.5}
+                color={"gray"}
+            />
+            {!collapsed && <span className={`text-sm grow ${colorScheme == 'dark' ? 'text-amber-50' : ""}`}>
+          {name}
+        </span>}
+            {/*{children}*/}
+        </Link>
+    )
 }
