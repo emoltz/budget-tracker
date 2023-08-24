023c76eec6b0e33c455eb5be53ed25792bb2eea6
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-24 18:16:28 -0400

    fix issue

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -1,91 +1,82 @@
 "use client";
-import BudgetByCategory from "@/components/BudgetByCategory";
-import { Category } from "@/lib/Interfaces";
-import {
-  FiHome,
-  FiTrendingUp,
-  FiCompass,
-  FiStar,
-  FiSettings,
-  FiMenu,
-  FiBell,
-  FiChevronDown,
-  FiPlus,
-} from "react-icons/fi";
-import { Button, Modal, Text } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { useAuth } from "@/app/context";
-import { getCategoriesNew } from "@/lib/firebase";
+import {Category, CategoryBudget} from "@/lib/Interfaces";
+import {FiPlus,} from "react-icons/fi";
+import {Button, Modal, Text} from "@mantine/core";
+import {useDisclosure} from "@mantine/hooks";
+import {useAuth} from "@/app/context";
+import {useCategoryBudgets_currentMonth} from "@/lib/firebase";
 
 export default function Budgets() {
-  //   const user = useAuth();
-  const { user, loading } = useAuth();
-  if (user) {
-    let categoryObjects = getCategoriesNew(user);
-    console.log(categoryObjects);
-  }
-  let categoryObjects = getCategoriesNew(user);
-  let categories = [
-    "Food/Drink",
-    "Groceries",
-    "Activities",
-    "Personal Items",
-    "Transportation",
-    "Home",
-    "Health",
-  ];
+    //   const user = useAuth();
+    const {user, loading} = useAuth();
+    const [opened, {open, close}] = useDisclosure(false);
+    const categoryBudgets: CategoryBudget[] | null = useCategoryBudgets_currentMonth(user)
 
-  // Dummy data
-  const Categories: Category[] | any = [
-    {
-      id: "1",
-      month: "January",
-      budget: 1000,
-      category_name: "Food",
-      year: 2023,
-      spent: 500,
-    },
-    {
-      id: "2",
-      month: "February",
-      budget: 1500,
-      category_name: "Rent",
-      year: 2023,
-      spent: 1200,
-    },
-    {
-      id: "3",
-      month: "March",
-      budget: 800,
-      category_name: "Entertainment",
-      year: 2023,
-      spent: 600,
-    },
-    {
-      id: "4",
-      month: "April",
-      budget: 2000,
-      category_name: "Transportation",
-      year: 2023,
-      spent: 1800,
-    },
-    {
-      id: "5",
-      month: "May",
-      budget: 1200,
-      category_name: "Utilities",
-      year: 2023,
-      spent: 1000,
-    },
-  ];
+    if (loading){
+        return <div>Loading...</div>
+    }
 
-  const [opened, { open, close }] = useDisclosure(false);
 
-  return (
-    <>
-      <h1>Budgets</h1>
-      <ul>
-        {/* {categories.map((category, index) => {
+    const categories = [
+        "Food/Drink",
+        "Groceries",
+        "Activities",
+        "Personal Items",
+        "Transportation",
+        "Home",
+        "Health",
+    ];
+
+    // Dummy data
+    const Categories: Category[] | any = [
+        {
+            id: "1",
+            month: "January",
+            budget: 1000,
+            category_name: "Food",
+            year: 2023,
+            spent: 500,
+        },
+        {
+            id: "2",
+            month: "February",
+            budget: 1500,
+            category_name: "Rent",
+            year: 2023,
+            spent: 1200,
+        },
+        {
+            id: "3",
+            month: "March",
+            budget: 800,
+            category_name: "Entertainment",
+            year: 2023,
+            spent: 600,
+        },
+        {
+            id: "4",
+            month: "April",
+            budget: 2000,
+            category_name: "Transportation",
+            year: 2023,
+            spent: 1800,
+        },
+        {
+            id: "5",
+            month: "May",
+            budget: 1200,
+            category_name: "Utilities",
+            year: 2023,
+            spent: 1000,
+        },
+    ];
+
+    return (
+        <>
+
+            <h1>Budgets</h1>
+            <ul>
+                {/* {categories.map((category, index) => {
           return (
             <BudgetByCategory
               key={index}
@@ -93,31 +84,31 @@ export default function Budgets() {
             ></BudgetByCategory>
           );
         })} */}
-      </ul>
-      <Button
-        leftIcon={<FiPlus />}
-        variant={"outline"}
-        color={"dark"}
-        compact
-        onClick={open}
-      >
-        New Category
-      </Button>
+            </ul>
+            <Button
+                leftIcon={<FiPlus/>}
+                variant={"outline"}
+                color={"dark"}
+                compact
+                onClick={open}
+            >
+                New Category
+            </Button>
 
-      <Modal
-        size={"xl"}
-        zIndex={1000}
-        opened={opened}
-        onClose={close}
-        title="Add New Budget Category"
-        centered
-      >
-        {/* Modal content */}
-        <Text>
-          This allows you to create a new budget category and assign it a budget
-          for the month.
-        </Text>
-      </Modal>
-    </>
-  );
+            <Modal
+                size={"xl"}
+                zIndex={1000}
+                opened={opened}
+                onClose={close}
+                title="Add New Budget Category"
+                centered
+            >
+                {/* Modal content */}
+                <Text>
+                    This allows you to create a new budget category and assign it a budget
+                    for the month.
+                </Text>
+            </Modal>
+        </>
+    );
 }
