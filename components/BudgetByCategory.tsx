f3a5368a2a14035afe5fa75990b14b7f2fd3e21c
Shamitbh <shamit.bhatia2@gmail.com>
2023-08-24 12:24:46 -0700

    changed budgets page to include paper / modal

--- components/BudgetByCategory.tsx
+++ components/BudgetByCategory.tsx
@@ -1,40 +1,28 @@
-import {Flex, Button, TextInput} from '@mantine/core';
-import { JsxElement } from 'typescript';
+import { Flex, Button, TextInput, Paper, Modal, Text } from "@mantine/core";
+import { JsxElement } from "typescript";
 import { IoFastFoodOutline } from "react-icons/io5";
 import {
-    FiHome,
-    FiTrendingUp,
-    FiCompass,
-    FiStar,
-    FiSettings,
-    FiMenu,
-    FiBell,
-    FiChevronDown,
-} from 'react-icons/fi';
-import {Category} from "@/lib/Interfaces";
+  FiHome,
+  FiTrendingUp,
+  FiCompass,
+  FiStar,
+  FiSettings,
+  FiMenu,
+  FiBell,
+  FiChevronDown,
+} from "react-icons/fi";
+import { Category } from "@/lib/Interfaces";
 
 export default function BudgetByCategory(props: any): JSX.Element {
-    // TODO add category interface
-    return (
-        <>
-            <Flex
-                mih={50}
-                bg="rgba(0, 150, 255, 0.8)"
-                gap="md"
-                justify="flex-start"
-                align="center"
-                direction="row"
-                wrap="wrap"
-                >
-                <IoFastFoodOutline></IoFastFoodOutline>
-                <p>{props.name}</p>
+  // TODO add category interface
+  return (
+    <>
+      <Paper shadow="xs" radius="xl" p="md" withBorder>
+        <IoFastFoodOutline></IoFastFoodOutline>
+        <p>{props.category}</p>
 
-                <TextInput
-                    type="number"
-                    placeholder="1000"
-                    label="Budget amount"
-                    />
-            </Flex>
-        </>
-    )
+        <TextInput type="number" placeholder="1000" label="Budget amount" />
+      </Paper>
+    </>
+  );
 }
