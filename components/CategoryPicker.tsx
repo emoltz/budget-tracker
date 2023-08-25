0ca745a2bbfab09a6c1fde37dd3581aa36867c3b
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-25 13:56:12 -0400

    new input, rollback Category Picker

--- components/CategoryPicker.tsx
+++ components/CategoryPicker.tsx
@@ -3,32 +3,43 @@ import {getUserCategories} from "@/lib/firebase";
 import {Select} from "@mantine/core";
 import React, {useEffect, useState} from "react";
 
-interface Props {
-    onCategoryChange: (category: string) => void,
-    [restProps : string] : any;
-}
-export function CategoryPicker({ onCategoryChange, ...restProps}: Props){
-    const {user} = useAuth();
- 
-    const [data, setData] = useState([]);
 
-    useEffect(() => {
-        getUserCategories(user)
-            .then((res) => setData(res))
-            .catch(console.error);
-    }, [user])
-   
-    return (
-        <Select data={data}
-                placeholder={"Select a category"}
-                maxDropdownHeight={160}
-                transitionProps={{duration: 150, transition: 'pop-top-left', timingFunction: 'ease'}}
-                dropdownComponent={"div"}
-                searchable
-                clearable
-                onChange={onCategoryChange}
-                value={restProps["value"] || ""}
-                error={restProps["error"]}
-        />
-    )
+export function CategoryPicker({ onCategoryChange }: {onCategoryChange: (category: string) => void}) {
+    interface Props {
+        onCategoryChange: (category: string) => void,
+
+        [restProps: string]: any;
+    }
+
+    export function CategoryPicker({onCategoryChange, ...restProps}: Props) {
+        const {user, loading} = useAuth();
+        //// const categories: Category[] = useCategories(user);
+        // const data = userCategories.map((category) => category.category_name);
+        // TODO add ability to create category from here!
+        ////const data: string[] = categories ? categories.map((category) => category.category_name) : [];
+
+
+        const [data, setData] = useState(["Test"]);
+
+        // not sure if there's a better way to get output from the async function
+        useEffect(() => {
+            getUserCategories(user)
+                .then((res) => setData(res))
+                .catch(console.error);
+        }, [user])
+
+        return (
+            <Select data={data}
+                    placeholder={"Select a category"}
+                    maxDropdownHeight={160}
+                    transitionProps={{duration: 150, transition: 'pop-top-left', timingFunction: 'ease'}}
+                    dropdownComponent={"div"}
+                    searchable
+                    clearable
+                    onChange={onCategoryChange}
+                    value={restProps["value"] || ""}
+                    error={restProps["error"]}
+            />
+        )
+    }
 }
--- components/MonthlyExpenses.tsx
+++ components/MonthlyExpenses.tsx
@@ -1,10 +1,15 @@
+"use client";
 import {Expense} from "@/lib/Interfaces";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table"
 import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input"
 
 import {IconPlus} from "@tabler/icons-react";
+import {useState} from "react";
 
 export default function MonthlyExpenses() {
+
+
     const monthlyExpensesSampleData: Expense[] = [
         {
             id: "1",
@@ -37,45 +42,114 @@ export default function MonthlyExpenses() {
             is_yearly: false,
         }
     ]
+    const [monthlyExpenses, setMonthlyExpenses] = useState<Expense[]>(monthlyExpensesSampleData);
+    const [showForm, setShowForm] = useState<boolean>(false);
+    const [newExpenseRow, setNewExpenseRow] = useState({
+        name: "",
+        category_name: "",
+        amount: "",
+        description: ""
+    });
+    const addNewExpense = () => {
+        const newExpense = {
+            id: Date.now().toString(),
+            ...newExpenseRow,
+            amount: parseFloat(newExpenseRow.amount),
+            is_monthly: true,
+            is_yearly: false
+        };
+        setMonthlyExpenses([...monthlyExpenses, newExpense]);
+        toggleForm();
+    }
+    const handleInputChange = (e, field) => {
+        setNewExpenseRow({
+            ...newExpenseRow,
+            [field]: e.target.value
+        });
+    }
 
+    const toggleForm = () => {
+        setShowForm(!showForm);
+    }
+    const handleSubmit = () => {
+        setMonthlyExpenses([...monthlyExpenses, {...newExpense, id: Date.now().toString()}]);
+        toggleForm();
+    }
     return (
-        <div>
-            <Table>
+        <div className={"ml-20 mr-20"}>
+            <Table className={""}>
 
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="w-[100px] text-left">Name</TableHead>
+                        <TableHead className=" text-left">Name</TableHead>
                         <TableHead className={"text-center"}>Category</TableHead>
-                        <TableHead className={"text-center w-[100px]"}>Amount</TableHead>
-                        <TableHead className={"w-[200px] text-center"}>Description</TableHead>
+                        <TableHead className={"text-center"}>Amount</TableHead>
+                        <TableHead className={" text-center"}>Description</TableHead>
+                        <TableHead className={" text-center"}>Action</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
                     {
-                        monthlyExpensesSampleData.map((expense, index) => {
+                        monthlyExpenses.map((expense, index) => {
                                 return (
                                     <TableRow key={index}>
                                         <TableCell className={"text-left"}>{expense.name}</TableCell>
                                         <TableCell>{expense.category_name}</TableCell>
                                         <TableCell>${expense.amount.toFixed(2)}</TableCell>
                                         <TableCell>{expense.description}</TableCell>
+                                        <TableCell>
+                                            <Button variant={"outline"}>...</Button>
+                                        </TableCell>
                                     </TableRow>
                                 )
                             }
                         )
                     }
+                    {
+                        showForm && (
+                            <TableRow>
+                                <TableCell>
+                                    <Input placeholder="Name" value={newExpenseRow.name}
+                                           onChange={(e) => handleInputChange(e, "name")}/>
+                                </TableCell>
+                                <TableCell>
+                                    <Input placeholder="Category" value={newExpenseRow.category_name}
+                                           onChange={(e) => handleInputChange(e, "category_name")}/>
+                                </TableCell>
+                                <TableCell>
+                                    <Input placeholder="Amount" value={newExpenseRow.amount}
+                                           onChange={(e) => handleInputChange(e, "amount")}/>
+                                </TableCell>
+                                <TableCell className={"w-[10px]"}>
+                                    <Input placeholder="Description" value={newExpenseRow.description}
+                                           onChange={(e) => handleInputChange(e, "description")}/>
+                                </TableCell>
+                                <TableCell>
+                                    <Button onClick={addNewExpense}
+                                    variant={"secondary"}
+                                    >
+                                        <IconPlus/>
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        )
+                    }
 
                 </TableBody>
             </Table>
+            { !showForm &&
+
             <div className={"text-right pt-5"}>
                 <Button
-                    variant={"outline"}
+                    variant={"secondary"}
+                    onClick={toggleForm}
                 >
 
                     <IconPlus/>
 
                 </Button>
             </div>
+            }
 
         </div>
     )
