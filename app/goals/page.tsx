d121a1110c5cad2ce412d3c17f24324096818079
Julianna <juliannagoldbas@gmail.com>
2023-11-22 13:04:56 -0500

    sort options for goals display

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -5,11 +5,22 @@ import { useGoals, addNewGoal } from "@/lib/firebase";
 import { Goal } from "@/lib/Interfaces"
 import GoalCard from "@/components/GoalCard";
 
-import { Grid, Card, Flex, Button, Color} from "@tremor/react";
+import { Grid, Card, Flex, Button, Color, Select, SelectItem } from "@tremor/react";
 import {useMantineColorScheme} from "@mantine/core";
 
 import AddGoalForm from "@/components/AddEditGoalForm";
 
+function sortByAmt(a: Goal, b: Goal) {
+    return a.amt_goal < b.amt_goal ? -1 : 1;
+}
+
+function sortByName(a: Goal, b: Goal) {
+    return a.goal_name < b.goal_name ? -1 : 1;
+}
+
+function sortByDate(a: Goal, b: Goal) {
+    return a.goal_date < b.goal_date ? -1 : 1;
+}
 
 export default function Page () {
     const {user, loading} = useAuth();
@@ -19,14 +30,30 @@ export default function Page () {
 
     const [showForm, setShowForm] = useState(false);
     const colors = ["amber", "indigo", "violet", "rose", "cyan"];
+    const [sortFn, setSortFn] = useState('date')
 
     return (
         <div className={colorScheme == "dark" ? "dark" : ""}>
-            <p>
-            Goals Page
-            </p>
+            <p>Goals Page</p>
+
+            <Flex flexDirection="row" className="w-1/3 justify-self-end">
+                <p>Sort By</p>
+                <Select value={sortFn} onValueChange={setSortFn} enableClear={false} >
+                    <SelectItem value='date'>
+                    Date
+                    </SelectItem>
+                    <SelectItem value='name'>
+                    Goal Name
+                    </SelectItem>
+                    <SelectItem value='amount'>
+                    Goal Amount
+                    </SelectItem>
+                </Select>
+            </Flex>
+
             <Grid numItems={1} numItemsMd={2} numItemsLg={3} className="gap-2 p-2">
-                {goals && goals.map((goal: Goal, idx: number) => {
+                {goals && goals.sort(sortFn === 'date' ? sortByDate : sortFn === 'name' ? sortByName : sortByAmt)
+                .map((goal: Goal, idx: number) => {
                     return (
                         <GoalCard
                             user={user}
